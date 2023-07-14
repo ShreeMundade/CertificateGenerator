ae1927a99da668b1daaad67eda9c1b1ab66200c9
Omkarjedhe8 <135060417+Omkarjedhe8@users.noreply.github.com>
2023-07-14 15:18:39 +0530

    organization service layer

--- src/main/java/com/renocrewsolutions/certgenerator/Service/ExamService.java
+++ src/main/java/com/renocrewsolutions/certgenerator/Service/ExamService.java
@@ -1,5 +1,45 @@
 package com.renocrewsolutions.certgenerator.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.renocrewsolutions.certgenerator.entity.Certification;
+import com.renocrewsolutions.certgenerator.entity.Exam;
+import com.renocrewsolutions.certgenerator.repository.CertificationRepository;
+import com.renocrewsolutions.certgenerator.repository.ExamRepository;
+
 public class ExamService {
+	
+	  private  ExamRepository examRepository;
+	private Long examId;
+	    @Autowired
+	    public ExamService( ExamRepository examRepository) {
+	        this. examRepository =  examRepository;
+	    }
+
+	    public List<Exam> getAllExam() {
+	        return  examRepository.findAll();
+	    }
+
+	    public Optional<Exam> getExamById(Long examId) {
+		
+			return examRepository.findById(examId);
+	    }
+
+	    public Exam createExam( Exam exam){
+	        return examRepository.save(exam);
+	    }
+	    
+
+	    public Exam updateExam(Exam exam) {
+	        return examRepository.save(exam);
+	    }
+
+	    public void deleteExam(Long examId) {
+	    	examRepository.deleteById(examId);
+	    }
+
 
 }
